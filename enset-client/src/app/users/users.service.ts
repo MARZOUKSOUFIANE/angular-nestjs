d5bfacab4121bb81d2caf87e21c8d3ef023d82be
soufiane <soufianemarzouk.2017@gmail.com>
2018-12-25 00:29:24 +0000

    l'ajout des dossier 'enset-server' et 'enset-client'

---
+++ enset-client/src/app/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/index";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private readonly BASE_URL = 'http://localhost:5000/users';
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable {
+    return this.http.get(this.BASE_URL);
+  }
+
+}
